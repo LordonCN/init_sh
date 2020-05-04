fc54e349661bb42dbc01f35faca76765c7e09924
Tcloser <xunjtech@gmail.com>
2020-05-04 21:29:05 +0800

    update readme + zsh.sh

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # init_sh
-for my unix system
+for my Unix system
+
+
+Notice:
+1. `.vim/` `.vimrc``.zshrc``.bash_profile``.config/` can replace directly
+
+2. `.ho-my-zsh` need to download from github.
+- highlighting plugin: 
+- autosuggestion plugin:
+```
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+```
+
+
+
+
+
---
+++ install/zshrc.sh
@@ -0,0 +1,14 @@
+#########################################################################
+# File Name: zshrc.sh
+# Author: Lordon
+# mail: xunjtech@gmail.com
+# Created Time: Mon May  4 21:24:30 2020
+#########################################################################
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+
