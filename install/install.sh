1f702265508f8afcf4aed4541476eb7dd4eaa2d8
Tcloser <xunjtech@gmail.com>
2020-05-04 21:49:59 +0800

    add install.sh

---
+++ install/install.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: zshrc.sh
+# Author: Lordon
+# mail: xunjtech@gmail.com
+# Created Time: Mon May  4 21:24:30 2020
+#########################################################################
+
+cp -r ~/init_sh/.vim ~/.vim  
+cp -r ~/init_sh/.config ~/.config
+cp ~/init_sh/.vimrc ~/.vimrc
+cp ~/init_sh/.zshrc ~/.zshrc
+cp ~/init_sh/.zsh_history ~/.zsh_history
+cp ~/init_sh/.bash_profile ~/.bash_profile
+
+# git configuration
+git config --global user.email "xunjtech@gmail.com"
+git config --global user.name "Lordon"
+
+
--- install/zshrc.sh
+++ install/zshrc.sh
@@ -5,6 +5,9 @@
 # Created Time: Mon May  4 21:24:30 2020
 #########################################################################
 
+# install zsh and oh-my-zsh and plugins
+brew install zsh
+chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
