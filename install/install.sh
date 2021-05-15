35610ada73099eca00ef390948586643ac81c5e2
xuluodong <xuluodong@white-rhino.auto>
2021-05-15 11:10:51 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -6,10 +6,10 @@
 #########################################################################
 
 cp -r ~/init_sh/.vim ~/.vim  
-cp -r ~/init_sh/.config ~/.config
+#cp -r ~/init_sh/.config ~/.config
 cp ~/init_sh/.vimrc ~/.vimrc
-cp ~/init_sh/.zshrc ~/.zshrc
-cp ~/init_sh/.zsh_history ~/.zsh_history
+#cp ~/init_sh/.zshrc ~/.zshrc
+#cp ~/init_sh/.zsh_history ~/.zsh_history
 cp ~/init_sh/.bash_profile ~/.bash_profile
 
 # git configuration
